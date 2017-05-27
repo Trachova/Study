034dd3b5e80284a202e28cfdab8d14b70e3475d0
Trachova <y.trachova@gmail.com>
2017-05-27 12:42:08 +0300

    autotest

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,8 +1,10 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.*;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static jdk.nashorn.internal.objects.NativeMath.random;
 
 /**
  * Created by X230 on 27.05.2017.
@@ -20,11 +22,33 @@ public class FirstTest {
 
     @Test
     public void firstTest() throws InterruptedException {
-        driver.get("http://magento.brainacad.com/");
+
         driver.findElement(By.cssSelector("#search")).sendKeys("PC games");
         Thread.sleep(5000);
     }
 
+    @Test
+    public void firstTest1() throws InterruptedException {
+        driver.get("http://magento.brainacad.com/english/customer/account/create/");
+        driver.findElement(By.cssSelector("#firstname")).sendKeys("July");
+        driver.findElement(By.cssSelector("#lastname")).sendKeys("Trachova");
+        final String email = random(10) + "@gmail.com";
+        driver.findElement(By.cssSelector("#email_address")).sendKeys(email);
+        System.out.println(email);
+        driver.findElement(By.cssSelector("#password")).sendKeys("631329");
+        driver.findElement(By.cssSelector("#confirmation")).sendKeys("631329");
+        driver.findElement(By.cssSelector("div.buttons-set > button")).click();
+
+    }
+
+    @Test
+    public void firstTest2() throws InterruptedException {
+        driver.get("http://magento.brainacad.com/english/customer/account/login/");
+        driver.findElement(By.cssSelector("#email")).sendKeys("0.1926362042845149@gmail.com");
+        driver.findElement(By.cssSelector("#pass")).sendKeys("631329");
+        driver.findElement(By.cssSelector("#send2")).click();
+
+    }
     /*@AfterMethod
     public void after() {
         driver.close();
